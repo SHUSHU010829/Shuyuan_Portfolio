2cbf0f084f6ced44d9e6926b8b87e2b3c38ab88b
ShuyuanChuang <shuyuanchuang2020@gmail.com>
2023-01-17 16:18:46 +0800

    [feat] navBar fin

--- src/components/Myself/index.js
+++ src/components/Myself/index.js
@@ -20,16 +20,18 @@ const Myself = (props) => {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       {/* ------- Nav ---------*/}
       <nav className="navbarItems">
+        <h1 className="navbar-logo">Shuyuan</h1>
         <div className="menu-icons" onClick={handleClick}>
           <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
         </div>
-        <ul className="nav-menu">
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
           <li><a href="#" className="nav-links">About</a></li>
           <li><a href="#" className="nav-links">Project</a></li>
           <li><a href="#" className="nav-links">Blog</a></li>
           <li> <a href="#" className="nav-links">Contact</a></li>
+          <li> <a href="#" className="nav-links-mobile">My Resume</a></li>
+          <button>My Resume</button>
         </ul>
-        <button>My Resume</button>
       </nav>
       {/* ------- Self ---------*/}
       <div className="container" style={{ marginTop: "150px" }}>
