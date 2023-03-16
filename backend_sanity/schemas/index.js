86f301dbc8855418c374584ad917920bc477349a
ShuyuanChuang <shuyuanchuang2020@gmail.com>
2023-03-16 11:17:22 +0800

    [feat] experience 區域初始

--- backend_sanity/schemas/index.js
+++ backend_sanity/schemas/index.js
@@ -1,4 +1,8 @@
 import work from './work'
 import about from './about'
+import skills from './skills'
+import workExperience from './workExperience'
+import experiences from './experiences'
+import testimonials from './testimonials'
 
-export const schemaTypes = [work, about]
+export const schemaTypes = [work, about, skills, workExperience, experiences, testimonials]
