69b12ca6565f75b02f4dd46719f4826dad4e41a6
ShuyuanChuang <shuyuanchuang2020@gmail.com>
2023-03-16 15:07:22 +0800

    [feat] 更換 icon [fix] 新增 Header Scroll down 樣式

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,7 +1,7 @@
 import email from '../assets/email.png';
 
 import circle from '../assets/circle.svg';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.svg';
 
 export default {
   email,
