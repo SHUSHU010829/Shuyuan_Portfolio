bcf14af3c41191d0b09a2888f0a2198416cffe1b
TonyPepe <41405045+TonyPepeBear@users.noreply.github.com>
2023-03-25 22:16:04 +0800

    [fix] 在螢幕寬不不足時無法開啟 Navbar  #1

--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from "react";
-import { HiMenuAlt4, HiX } from 'react-icons/hi';
-import { motion } from 'framer-motion';
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
 
 import "./Navbar.scss";
-import { images } from '../../constants';
+import { images } from "../../constants";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const variants = {
+    open: { x: 0 },
+    closed: { x: "200%" },
+  };
 
   return (
     <nav className="app__navbar">
@@ -24,24 +28,23 @@ const Navbar = () => {
 
       <div className="app__navbar-menu">
         <HiMenuAlt4 onClick={() => setToggle(true)} />
-
-        {toggle && (
-          <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
-          >
-            <HiX onClick={() => setToggle(false)} />
-            <ul>
-              {['home', 'about', 'work', 'skills', 'contact'].map((item) => (
-                <li key={item}>
-                  <a href={`#${item}`} onClick={() => setToggle(false)}>
-                    {item}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </motion.div>
-        )}
+        <motion.div
+          animate={toggle ? "open" : "closed"}
+          initial={false}
+          variants={variants}
+          transition={{ duration: 0.85, ease: "easeOut" }}
+        >
+          <HiX onClick={() => setToggle(false)} />
+          <ul>
+            {["home", "about", "work", "skills", "contact"].map((item) => (
+              <li key={item}>
+                <a href={`#${item}`} onClick={() => setToggle(false)}>
+                  {item}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </motion.div>
       </div>
     </nav>
   );
